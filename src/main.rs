962269f0ac46e1db1cf21294f700e0c1610afd67
me-it-is <188520719+me-it-is@users.noreply.github.com>
2025-08-02 14:57:48 -0700

    refactor read_header

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use std::{fs::{File}, io::{Read}};
 static WPILOG_PATH: &str = "test.wpilog";
 
 struct Wpilog {
-    header: Header
+    header: FileHeader
 }
 
 #[derive(Debug, Clone)]
 enum WpilogReadErrors {
     FileDoesNotExist,
-    ReadError
+    ReadError,
+    NoDataLeft,
+    InvalidHeader
 }
 
 struct Record {
@@ -18,7 +20,7 @@ struct Record {
     data: Vec<u8>
 }
 
-struct Header {
+struct FileHeader {
     version_number: u16,
     extra_string: String
 }
@@ -35,28 +37,30 @@ fn read_wpilog(path: &str) -> Result<Wpilog, WpilogReadErrors> {
     return Ok(Wpilog { header: header });
 }
 
-fn read_header(file: &mut (Vec<u8>, usize)) -> Result<Header, WpilogReadErrors> {
-    assert!(match str::from_utf8(next_chunk(file, 6).as_slice()) {Ok(s) => s, Err(_) => return Err(WpilogReadErrors::ReadError)} == "WPILOG");
+fn read_header(file: &mut (Vec<u8>, usize)) -> Result<FileHeader, WpilogReadErrors> {
+    if (match str   ::from_utf8(match next_chunk(file, 6) {Ok(c) => c, Err(_) => return Err(WpilogReadErrors::ReadError)}.as_slice()) {Ok(s) => s, Err(_) => return Err(WpilogReadErrors::ReadError)} != "WPILOG") {
+        return Err(WpilogReadErrors::InvalidHeader);
+    }
 
-    let version_number = u16::from_le_bytes(next_chunk(file, 2).try_into().unwrap());
+    let version_number = u16::from_le_bytes(match next_chunk(file, 2) {Ok(c) => c, Err(_) => return Err(WpilogReadErrors::InvalidHeader)}.try_into().unwrap());
 
-    let extra_string_length = u32::from_le_bytes(next_chunk(file, 4).try_into().unwrap());
+    let extra_string_length = u32::from_le_bytes(next_chunk(file, 4)?.try_into().unwrap());
 
-    let extra_string_raw = next_chunk(file, extra_string_length as usize);
-    let extra_string = match str::from_utf8(extra_string_raw.as_slice()) {Ok(s) => s, Err(_) => return  Err(WpilogReadErrors::ReadError)};
+    let extra_string_raw = next_chunk(file, extra_string_length as usize)?;
+    let extra_string = match str::from_utf8(extra_string_raw.as_slice()) {Ok(s) => s, Err(_) => return  Err(WpilogReadErrors::InvalidHeader)};
     
-    return Ok(Header { version_number: version_number, extra_string: extra_string.to_string()})
+    return Ok(FileHeader { version_number: version_number, extra_string: extra_string.to_string()})
 }
 
 fn next_chunk(file: &mut (Vec<u8>, usize),size: usize) -> Vec<u8> {
     let mut out: Vec<u8> =  Vec::new();
     let mut iter = file.0.iter().skip(file.1);
     for _i in 0..size {
-        out.push(*iter.next().unwrap());
+        out.push(*(match iter.next() {Some(n) => n, None => return Err(WpilogReadErrors::NoDataLeft)}));
     } 
     file.1 += size;
 
-    return out;
+    return Ok(out);
 }
 
 
