e6e32fac59d0977273c5dc07d2f3afee57c199a5
me-it-is <188520719+me-it-is@users.noreply.github.com>
2025-08-06 12:28:04 -0700

    reorg

---
+++ src/records.rs
@@ -0,0 +1,254 @@
+pub mod records {
+use std::time::Duration;
+use std::collections::HashMap;
+
+use crate::shared::shared::{WpilogReadErrors, pad_to_n_bytes, next_chunk};
+use crate::control_records::control_records::{StartRecordData, FinishRecordData, SetMetaDataRecordData, process_control_record};
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct EntryData {
+    pub start_record_index: u32,
+    name: String,
+    data_type: DataType,
+    pub metadata: Metadata,
+    pub finish_record_index: Option<u32>,
+}
+
+impl EntryData {
+    pub fn new(start_record_index: u32, name: String, data_type: DataType, metadata: Metadata) -> EntryData{
+        EntryData { start_record_index, name, data_type, metadata, finish_record_index: None }
+    }
+}
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Metadata {
+    metadata: Option<serde_json::Value>,
+}
+
+
+#[allow(dead_code)]
+pub struct Record {
+    entry_id: u32,
+    time_stamp: Duration,
+    data: RecordData,
+}
+#[allow(dead_code)]
+pub enum RecordData {
+    Start(StartRecordData),
+    Finish(FinishRecordData),
+    SetMetadata(SetMetaDataRecordData),
+    Raw(Vec<u8>),
+    Boolean(bool),
+    Integer(i64),
+    Float(f32),
+    Double(f64),
+    String(String),
+    BooleanArray(Vec<bool>),
+    IntegerArray(Vec<i64>),
+    FloatArray(Vec<f32>),
+    DoubleArray(Vec<f64>),
+    StringArray(Vec<String>),
+    Json(Option<serde_json::Value>),
+    MessagePack(Vec<u8>),
+    Struct(Vec<u8>),
+    StructArray(Vec<u8>),
+    PhotonStruct(Vec<u8>),
+    ProtoBuff(Vec<u8>),
+}
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+pub enum DataType {
+    Raw,
+    Boolean,
+    Integer,
+    Float,
+    Double,
+    String,
+    BooleanArray,
+    IntegerArray,
+    FloatArray,
+    DoubleArray,
+    StringArray,
+    Json,
+    MessagePack,
+    Struct(String),
+    StructArray(String),
+    PhotonStruct(String),
+    ProtoBuff(String),
+}
+    fn get_current_entry_data(
+    entry_data_lut: &HashMap<u32, Vec<EntryData>>,
+    current_record: u32,
+    entry_id: u32,
+) -> Result<&EntryData, WpilogReadErrors> {
+    let current_id_data = match entry_data_lut.get(&entry_id) {
+        Some(l) => l,
+        None => return Err(WpilogReadErrors::UseOfEntryIdWithoutStart),
+    };
+
+    for data in current_id_data {
+        match data.finish_record_index {
+            None => return Ok(data),
+            Some(i) => {
+                if i < current_record {
+                    return Ok(data);
+                }
+            }
+        }
+    }
+
+    return Err(WpilogReadErrors::UseOfEntryIdAfterFinish);
+}
+
+pub fn read_next_record(
+    file: &mut (Vec<u8>, usize),
+    entry_data_lut: &mut HashMap<u32, Vec<EntryData>>,
+    current_record: u32,
+) -> Result<Record, WpilogReadErrors> {
+    let header_bit_field = next_chunk(file, 1)?[0];
+
+    let entry_id = u32::from_le_bytes(pad_to_n_bytes(next_chunk(
+        file,
+        ((header_bit_field & 0b000011) + 1) as usize,
+    )?));
+
+    let raw_payload_size = pad_to_n_bytes(next_chunk(
+        file,
+        (((header_bit_field & 0b00001100) >> 2) + 1) as usize,
+    )?);
+    let payload_size = u32::from_le_bytes(raw_payload_size);
+
+    let timestamp = u64::from_le_bytes(pad_to_n_bytes(next_chunk(
+        file,
+        (((header_bit_field & 0b01110000) >> 4) + 1) as usize,
+    )?));
+
+    let data;
+
+    if entry_id == 0 {
+        data = process_control_record(file, current_record, entry_data_lut)?;
+    } else {
+        let raw_data = next_chunk(file, payload_size as usize)?;
+        data = process_data_from_standard_record(
+            &get_current_entry_data(entry_data_lut, current_record, entry_id)?.data_type,
+            raw_data,
+        )?;
+    }
+
+    return Ok(Record {
+        entry_id: entry_id as u32,
+        time_stamp: Duration::from_micros(timestamp),
+        data: data,
+    });
+}
+
+fn process_boolean(byte: [u8; 1]) -> Result<bool, WpilogReadErrors> {
+    return match byte[0] {
+        0 => Ok(false),
+        1 => Ok(true),
+        _ => return Err(WpilogReadErrors::MalformedData),
+    };
+}
+
+fn process_data_from_standard_record(
+    data_type: &DataType,
+    data: Vec<u8>,
+) -> Result<RecordData, WpilogReadErrors> {
+    return Ok(match data_type {
+        DataType::Raw => RecordData::Raw(data),
+        DataType::Boolean => RecordData::Boolean(process_boolean(data.try_into().unwrap())?),
+        DataType::Integer => RecordData::Integer(i64::from_le_bytes(data.try_into().unwrap())),
+        DataType::Float => RecordData::Float(f32::from_le_bytes(data.try_into().unwrap())),
+        DataType::Double => RecordData::Double(f64::from_le_bytes(data.try_into().unwrap())),
+        DataType::String => RecordData::String(match String::from_utf8(data) {
+            Ok(s) => s,
+            Err(_) => return Err(WpilogReadErrors::MalformedData),
+        }),
+        DataType::BooleanArray => {
+            RecordData::BooleanArray(process_array_data(data, &(process_boolean))?)
+        }
+        DataType::IntegerArray => {
+            RecordData::IntegerArray(process_array_data_no_err(data, &i64::from_le_bytes)?)
+        }
+        DataType::FloatArray => {
+            RecordData::FloatArray(process_array_data_no_err(data, &f32::from_le_bytes)?)
+        }
+        DataType::DoubleArray => {
+            RecordData::DoubleArray(process_array_data_no_err(data, &f64::from_le_bytes)?)
+        }
+        DataType::StringArray => match process_string_array(data) {
+            Ok(sa) => sa,
+            Err(_) => return Err(WpilogReadErrors::MalformedData),
+        },
+        DataType::Json => RecordData::Json(match data.len() {
+            0 => None,
+            _ => match serde_json::from_slice(&data) {
+                Ok(j) => j,
+                Err(_) => return Err(WpilogReadErrors::MalformedData),
+            },
+        }),
+        DataType::MessagePack => RecordData::MessagePack(data),
+        DataType::Struct(_) => RecordData::Struct(data),
+        DataType::StructArray(_) => RecordData::StructArray(data),
+        DataType::PhotonStruct(_) => RecordData::PhotonStruct(data),
+        DataType::ProtoBuff(_) => RecordData::ProtoBuff(data),
+    });
+}
+
+fn process_array_data_no_err<T, const DATA_SIZE: usize>(
+    data: Vec<u8>,
+    from_func: &dyn Fn([u8; DATA_SIZE]) -> T,
+) -> Result<Vec<T>, WpilogReadErrors> {
+    return process_array_data(data, &|data: [u8; DATA_SIZE]| Ok(from_func(data)));
+}
+
+fn process_array_data<T, const DATA_SIZE: usize>(
+    data: Vec<u8>,
+    from_func: &dyn Fn([u8; DATA_SIZE]) -> Result<T, WpilogReadErrors>,
+) -> Result<Vec<T>, WpilogReadErrors> {
+    let mut out = Vec::new();
+    let mut entries = data.chunks_exact(DATA_SIZE);
+
+    loop {
+        match entries.next() {
+            Some(e) => out.push(from_func(e.try_into().unwrap())?),
+            None => break,
+        }
+    }
+    if entries.remainder().len() != 0 {
+        return Err(WpilogReadErrors::MalformedData);
+    }
+    return Ok(out);
+}
+
+fn process_string_array(data: Vec<u8>) -> Result<RecordData, WpilogReadErrors> {
+    let mut indexer = (data, 0);
+    let length = u32::from_le_bytes(next_chunk(&mut indexer, 4)?.try_into().unwrap());
+    let mut out = Vec::new();
+
+    for _i in 0..length {
+        let string_length = u32::from_le_bytes(next_chunk(&mut indexer, 4)?.try_into().unwrap());
+        out.push(
+            match String::from_utf8(next_chunk(&mut indexer, string_length as usize)?) {
+                Ok(s) => s,
+                Err(_) => return Err(WpilogReadErrors::MalformedData),
+            },
+        );
+    }
+
+    return Ok(RecordData::StringArray(out));
+}
+
+pub fn process_metadata(data: Vec<u8>) -> Result<Metadata, WpilogReadErrors> {
+    let metadata;
+    if data.len() == 0 {
+        metadata = None
+    } else {
+        metadata = Some(match serde_json::from_slice(&data) {
+            Ok(j) => j,
+            Err(_) => return Err(WpilogReadErrors::MalformedData),
+        });
+    }
+    return Ok(Metadata { metadata });
+}
+}
---
+++ src/shared.rs
@@ -0,0 +1,43 @@
+pub mod shared {
+#[derive(Debug, Clone)]
+pub enum WpilogReadErrors {
+    FileDoesNotExist,
+    ReadError,
+    NoDataLeft,
+    InvalidHeader,
+    InvalidRecoard,
+    UnsupportedWpilogVersion,
+    MalformedData,
+    UseOfEntryIdWithoutStart,
+    UseOfEntryIdAfterFinish,
+    EntryIdAlreadyStarted,
+    SetMetadataWithoutStart,
+    FinishWithoutStart,
+}
+pub fn pad_to_n_bytes<const SIZE: usize>(data: Vec<u8>) -> [u8; SIZE] {
+    let mut arr: [u8; SIZE] = [0; SIZE];
+    for i in 0..SIZE {
+        arr[i] = match data.get(i) {
+            Some(n) => *n,
+            None => 0,
+        };
+    }
+    return arr;
+}
+
+pub fn next_chunk(file: &mut (Vec<u8>, usize), size: usize) -> Result<Vec<u8>, WpilogReadErrors> {
+    let mut out: Vec<u8> = Vec::new();
+    let mut iter = file.0.iter().skip(file.1);
+    for _i in 0..size {
+        out.push(
+            *(match iter.next() {
+                Some(n) => n,
+                None => return Err(WpilogReadErrors::NoDataLeft),
+            }),
+        );
+    }
+    file.1 += size;
+
+    return Ok(out);
+}
+}
