16d6135ac706854a559831a0834265c8a269c7af
me-it-is <188520719+me-it-is@users.noreply.github.com>
2025-08-06 15:06:56 -0700

    add record refs by entry and run cargo fmt

--- src/shared.rs
+++ src/shared.rs
@@ -1,43 +1,46 @@
 pub mod shared {
-#[derive(Debug, Clone)]
-pub enum WpilogReadErrors {
-    FileDoesNotExist,
-    ReadError,
-    NoDataLeft,
-    InvalidHeader,
-    InvalidRecoard,
-    UnsupportedWpilogVersion,
-    MalformedData,
-    UseOfEntryIdWithoutStart,
-    UseOfEntryIdAfterFinish,
-    EntryIdAlreadyStarted,
-    SetMetadataWithoutStart,
-    FinishWithoutStart,
-}
-pub fn pad_to_n_bytes<const SIZE: usize>(data: Vec<u8>) -> [u8; SIZE] {
-    let mut arr: [u8; SIZE] = [0; SIZE];
-    for i in 0..SIZE {
-        arr[i] = match data.get(i) {
-            Some(n) => *n,
-            None => 0,
-        };
+    #[derive(Debug, Clone)]
+    pub enum WpilogReadErrors {
+        FileDoesNotExist,
+        ReadError,
+        NoDataLeft,
+        InvalidHeader,
+        InvalidRecoard,
+        UnsupportedWpilogVersion,
+        MalformedData,
+        UseOfEntryIdWithoutStart,
+        UseOfEntryIdAfterFinish,
+        EntryIdAlreadyStarted,
+        SetMetadataWithoutStart,
+        FinishWithoutStart,
     }
-    return arr;
-}
-
-pub fn next_chunk(file: &mut (Vec<u8>, usize), size: usize) -> Result<Vec<u8>, WpilogReadErrors> {
-    let mut out: Vec<u8> = Vec::new();
-    let mut iter = file.0.iter().skip(file.1);
-    for _i in 0..size {
-        out.push(
-            *(match iter.next() {
-                Some(n) => n,
-                None => return Err(WpilogReadErrors::NoDataLeft),
-            }),
-        );
+    pub fn pad_to_n_bytes<const SIZE: usize>(data: Vec<u8>) -> [u8; SIZE] {
+        let mut arr: [u8; SIZE] = [0; SIZE];
+        for i in 0..SIZE {
+            arr[i] = match data.get(i) {
+                Some(n) => *n,
+                None => 0,
+            };
+        }
+        return arr;
     }
-    file.1 += size;
 
-    return Ok(out);
-}
+    pub fn next_chunk(
+        file: &mut (Vec<u8>, usize),
+        size: usize,
+    ) -> Result<Vec<u8>, WpilogReadErrors> {
+        let mut out: Vec<u8> = Vec::new();
+        let mut iter = file.0.iter().skip(file.1);
+        for _i in 0..size {
+            out.push(
+                *(match iter.next() {
+                    Some(n) => n,
+                    None => return Err(WpilogReadErrors::NoDataLeft),
+                }),
+            );
+        }
+        file.1 += size;
+
+        return Ok(out);
+    }
 }
